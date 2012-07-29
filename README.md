443dad4e004840a3a06ce43692fb57723e75887d
SMbey0nd <SMbey0nd@163.com>
2012-07-29 18:04:31 +0800

    creat the directory and the files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Detect.js
+
+统一环境检测包。
+
+---
+
+## Network
+## UA
+## Ability
+## Hardware
+### Resolution
+### Performance
+## DeviceAPI
+
+```js
+代码示例
+```
---
+++ src/detect.js
@@ -0,0 +1,15 @@
+/* 统一环境检测包
+ * By SMbey0nd http://github.com/SMbey0nd
+ * MIT Licensed.
+ */
+// --------------------------------
+// Thanks to:
+//  - https://github.com/lognormal/boomerang/
+//
+// TODO: 
+//  - 
+
+(function(w) {
+	//
+	
+}(window));
